4457814ba71084d45baef6fce20560fc1e0f5aa7
vagrant <vagrant@homestead>
2020-12-06 14:25:54 +0000

    管理员可删除用户

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -11,7 +11,7 @@ class UsersController extends Controller
     public function __construct()
     {
         $this->middleware('auth', [
-            'except' => ['create', 'store', 'index']
+            'except' => ['show', 'create', 'store', 'index']
         ]);
 
         $this->middleware('guest', [
@@ -64,6 +64,7 @@ class UsersController extends Controller
         return view('users.edit', compact('user'));
     }
 
+    // 更新用户信息
     public function update(User $user, Request $request)
     {
         $this->authorize('update', $user);
@@ -82,5 +83,14 @@ class UsersController extends Controller
         session()->flash('success', '个人资料更新成功！');
 
         return redirect()->route('users.show', $user->id);
+    }
+
+    // 删除用户
+    public function destroy(User $user)
+    {
+        $this->authorize('destroy', $user);
+        $user->delete();
+        session()->flash('success', '成功删除用户！');
+        return back();
     }
 }
--- app/Policies/UserPolicy.php
+++ app/Policies/UserPolicy.php
@@ -24,8 +24,8 @@ class UserPolicy
         return $currentUser->id === $user->id;
     }
 
-    public function test(User $currentUser, User $user)
+    public function destroy(User $currentUser, User $user)
     {
-        return ($currentUser->id === $user->id) && ($currentUser->email === $user->email);
+        return $currentUser->is_admin && $currentUser->id !== $user->id;
     }
 }
